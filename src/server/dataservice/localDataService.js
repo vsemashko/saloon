37101d2b67e7013b5c979357bf77294568e64116
Vladimir Semashko <vladimir.semashko@intexsoft.by>
2015-07-12 21:58:59 +0300

    Add pump configs, refactor data services

---
+++ src/server/dataservice/localDataService.js
@@ -0,0 +1,20 @@
+var LocalDataService = function () {
+
+    var vm = this;
+    vm.jsonfileservice = require('./utils/jsonfileservice')();
+    vm.getCocktails = getCocktails;
+    vm.getPumpConfiguration = getPumpConfiguration;
+
+    //////////////////
+
+    function getCocktails() {
+        return vm.jsonfileservice.getJsonFromFile('./../../data/coctails.json');
+    }
+
+    function getPumpConfiguration(req, res, next) {
+        return vm.jsonfileservice.getJsonFromFile('./../../data/pumpConfig.json');
+    }
+
+};
+
+module.exports = LocalDataService;
