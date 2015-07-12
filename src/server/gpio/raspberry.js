6430caa0bfee0898740a580a5d82abd33d52ba1d
Vladimir Semashko <vladimir.semashko@intexsoft.by>
2015-07-12 05:15:06 +0300

    Add water flow events

--- src/server/gpio/raspberry.js
+++ src/server/gpio/raspberry.js
@@ -5,11 +5,13 @@ var Q = require('q');
 
 var Raspberry = function () {
     var PUMP_PIN = 3;
+    var WATER_FLOW_SENSOR_PIN = 23;
 
     var vm = this;
     vm.pour = pour;
 
     process.on('exit', cleanupGPIO);
+    gpio.on('change', processFlowChanges);
 
     activate();
 
@@ -18,6 +20,7 @@ var Raspberry = function () {
     function activate() {
         gpio.setMode(gpio.MODE_BCM);
         gpio.setup(PUMP_PIN, gpio.DIR_OUT, callback);
+        gpio.setup(WATER_FLOW_SENSOR_PIN, gpio.DIR_IN, callback);
     }
 
     function callback(data) {
@@ -36,6 +39,10 @@ var Raspberry = function () {
             deferred.resolve();
         }, 3000);
         return deferred.promise;
+    }
+
+    function processFlowChanges(channel, value) {
+        console.log('Channel ' + channel + ' value is now ' + value);
     }
 
     function cleanupGPIO() {
