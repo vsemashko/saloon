7609de9e61c4877d4c52b6b2be74fcb99f153eb4
Vladimir Semashko <vladimir.semashko@intexsoft.by>
2015-08-01 22:17:07 +0300

    Fix raspberry flow measurements

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -16,8 +16,8 @@ var Routes = function (app) {
     }
 
     function pourLiquid(req, res, next) {
-        vm.raspberry.pour('Whiskey', 30).then(function () {
-            vm.raspberry.pour('Cola', 30).then(function () {
+        vm.raspberry.pour('Whiskey', parseInt(req.query.amount)).then(function () {
+            vm.raspberry.pour('Cola', req.query.amount).then(function () {
                 res.send({result: 'Liquid is ready!'});
             });
         });
