5014948ee445603844a69b18f3ccc9e6df09867a
Vadim Chadyuk <vadim.chadyuk@intexsoft.by>
2015-08-10 20:58:34 +0300

    Добавил сплэш скрин и команду "Поехали" для активации

--- src/client/app/layout/shell.js
+++ src/client/app/layout/shell.js
@@ -5,9 +5,9 @@
         .module('app.layout')
         .controller('Shell', Shell);
 
-    Shell.$inject = ['$timeout', 'config', 'logger', 'dataservice'];
+    Shell.$inject = ['$rootScope', 'config', 'logger', 'dataservice'];
 
-    function Shell($timeout, config, logger, dataservice) {
+    function Shell($rootScope, config, logger, dataservice) {
         /*jshint validthis: true */
         var vm = this;
 
@@ -15,18 +15,43 @@
         vm.busyMessage = 'Please wait ...';
         vm.isBusy = true;
         vm.showSplash = true;
+        vm.hideSplash = hideSplash;
+
+        $rootScope.safeApply = function(fn) {
+            var phase = this.$root.$$phase;
+            if(phase == '$apply' || phase == '$digest') {
+                if(fn && (typeof(fn) === 'function')) {
+                    fn();
+                }
+            } else {
+                this.$apply(fn);
+            }
+        };
 
         activate();
 
         function activate() {
+            
             logger.success(config.appTitle + ' loaded!', null);
             dataservice.ready().then(function () {
-                hideSplash();
+                var commands = [];
+                commands = {
+                    'поехали': hideSplash
+                };
+                // Add our commands to annyang
+                annyang.addCommands(commands);
+                // Start listening.
+                annyang.debug(true);
+                annyang.setLanguage("ru");
+                annyang.start();
             });
         }
 
         function hideSplash() {
-            vm.showSplash = false;
+            $rootScope.safeApply(function() {
+                vm.showSplash = false;
+                annyang.pause();
+            });
         }
     }
 })();
